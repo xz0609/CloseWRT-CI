b22302d66c1be0d66f346eb170ad0fc27d7856f1
xz0609 <xz0609@126.com>
2025-08-12 11:08:23 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -71,6 +71,9 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main" "" "luci-app-lucky"
+UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "" "lucky"
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
